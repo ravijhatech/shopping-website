c54a5377f3778a3954866598895d0e5bebc8514c
ravijhatech <ravijha1331998@gmail.com>
2025-05-04 09:05:12 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  name:String,
+  fullName:String,
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   otp:String,
