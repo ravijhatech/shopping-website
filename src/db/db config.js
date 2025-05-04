0877043b8bf7809a6942b8ad8577f9a32aa45e19
ravijhatech <ravijha1331998@gmail.com>
2025-05-04 09:08:00 +0530

    Update db config.js

--- src/db/db config.js
+++ src/db/db config.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect("mongodb+srv://ravipratihast71:jPoRfADyx0J54rU3@cluster0.hkwcuwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/ApnaShppongMart");
   } catch (err) {
     console.error('MongoDB connection error:', err);
-    process.exit(1);
+
   }
 };
 
