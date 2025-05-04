dcd1b5c3d418aaa2f2bc0a8f00c87b33372b8fa4
ravijhatech <ravijha1331998@gmail.com>
2025-05-05 00:37:50 +0530

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -64,12 +64,12 @@ export const changePasswordByEmail = async (req, res) => {
 
 
 export const register = async (req, res) => {
-  const { fullName,email, password } = req.body;
+  const { name,email, password } = req.body;
   try {
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: 'User already exists' });
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ fullName,email, password:hashedPassword });
+    const user = new User({ name,email, password:hashedPassword });
     await user.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
